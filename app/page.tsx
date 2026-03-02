f2bb736d7fc0d2e998567ed257ce1087526f8f1e
Nikola Vuletić <nikolavuletic412@gmail.com>
2026-03-03 00:54:03 +0100

    layout.tsx for React Query

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
+'use client';
+
 import Navbar from "@/components/Navbar";
-
-
-// https://api.openweathermap.org/data/2.5/weather?q=pune&APPID=NEXT_PUBLIC_WEATHER_KEY   
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export interface WeatherResponse {
   coord: {
     lon: number;
     lat: number;
   };
-
   weather: {
     id: number;
     main: string;
     description: string;
     icon: string;
   }[];
-
   base: string;
-
   main: {
     temp: number;
     feels_like: number;
@@ -25,24 +23,19 @@ export interface WeatherResponse {
     temp_max: number;
     pressure: number;
     humidity: number;
-    sea_level: number;
-    grnd_level: number;
+    sea_level?: number;
+    grnd_level?: number;
   };
-
   visibility: number;
-
   wind: {
     speed: number;
     deg: number;
-    gust: number;
+    gust?: number;
   };
-
   clouds: {
     all: number;
   };
-
   dt: number;
-
   sys: {
     type: number;
     id: number;
@@ -50,7 +43,6 @@ export interface WeatherResponse {
     sunrise: number;
     sunset: number;
   };
-
   timezone: number;
   id: number;
   name: string;
@@ -58,9 +50,30 @@ export interface WeatherResponse {
 }
 
 export default function Home() {
+
+  const { isPending, error, data } = useQuery<WeatherResponse>({
+    queryKey: ["weather", "pune"],
+    queryFn: async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&units=metric`;
+
+      const response = await axios.get<WeatherResponse>(url);
+      return response.data;
+    },
+  });
+
+  console.log("data", data);
+
+  if (isPending) return <div>Loading...</div>;
+  if (error) return <div>Error loading weather</div>;
+
   return (
-    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen" > 
-      <Navbar /> 
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+      <Navbar />
+      <div className="p-4">
+        <h1>{data?.name}</h1>
+        <p>{data?.main.temp} °C</p>
+        <p>{data?.weather[0].description}</p>
+      </div>
     </div>
   );
 }
